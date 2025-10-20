8eaa8c717dd35f32f526718297d3c3cc32421dd3
spandu-creative <spandanas320@gmail.com>
2025-10-20 22:41:38 +0530

    nodejsDemoapp

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from CI/CD Pipeline using GitHub Actions and Docker!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App running on port ${port}`));
